400a958f29868d3b789cc652c245f4424ca5a4b7
ShxDrag <bns19126@gmail.com>
2025-12-03 17:34:31 +0900

    Update SPANWADM.lua

--- SPANWADM.lua
+++ SPANWADM.lua
@@ -1,1 +1,8 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ShxDrag/Scripty/refs/heads/main/ADMMOPSPAWN.lua"))()
+
+-- Roblox notification (Copy All And Paste To Your Ex3cutor)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Script Executed";
+    Text = "AntiBan Bypass On 🟩";
+    Duration = 5;
+})
