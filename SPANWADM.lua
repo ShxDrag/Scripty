1a9ef9b87de66e7b31c661533e2d761ede1f75e0
ShxDrag <bns19126@gmail.com>
2026-01-10 19:16:17 +0900

    Update SPANWADM.lua

--- SPANWADM.lua
+++ SPANWADM.lua
@@ -1,2 +1,1 @@
--- Ver 2.0 New Update! Copy And Past3 it to your 3xecut0r
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ShxDrag/Scripty/refs/heads/main/ADMMOPSPAWN.lua"))()
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/ShxDrag/Scripty/refs/heads/main/ADMMOPSPAWN.lua"))()
